c2cb022068ac429c9fc9e91bdfc8db0b694b54b6
Sayed Mubarish <mubarish007@gmail.com>
2025-07-24 17:50:56 +0530

    set up a basic server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const dotenv = require("dotenv")
+
+
+const app = express()
+dotenv.config({ quiet: true })
+
+app.get("/",(req,res)=>{
+    res.sendStatus(200)
+})
+
+app.listen(process.env.PORT ,()=>{
+    console.log(`Listening on port no : ${process.env.PORT}`);
+})
