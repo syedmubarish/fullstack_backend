235af5da37ff904dc830dd61928b3937be517d6a
Sayed Mubarish <mubarish007@gmail.com>
2025-07-25 09:35:07 +0530

    use object destructing

--- config/db.js
+++ config/db.js
@@ -1,16 +1,18 @@
-const Sequelize = require("sequelize")
+const Sequelize = require("sequelize");
 
+const sequelize = new Sequelize("cruddb", "root", process.env.sqlpassword, {
+  dialect: "mysql",
+});
 
-const sequelize = new Sequelize('cruddb','root',process.env.sqlpassword,{
-    dialect : 'mysql'
-})
+function dbAuth() {
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Connected to crud db");
+    })
+    .catch((err) => {
+      console.log("Connection failed :", err);
+    });
+}
 
-
-
-sequelize.authenticate()
-.then(()=>{
-    console.log("Connected to crud db");
-})
-.catch((err)=>{
-    console.log("Connection failed :",err);
-})
+module.exports = { sequelize, dbAuth };
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require("express")
-const dotenv = require("dotenv")
-dotenv.config({ quiet: true })
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ quiet: true });
 
-require("./config/db")
-const app = express()
+const { sequelize, dbAuth } = require("./config/db");
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.sendStatus(200)
-})
+dbAuth();
 
-app.listen(process.env.PORT ,()=>{
-    console.log(`Listening on port:${process.env.PORT}`);
-})
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port:${process.env.PORT}`);
+});
