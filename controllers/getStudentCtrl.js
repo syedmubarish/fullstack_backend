484d613285120a3f74517050d71b7277bf74a50b
Sayed Mubarish <mubarish007@gmail.com>
2025-07-25 16:28:05 +0530

    implement getStudentDetails

---
+++ controllers/getStudentCtrl.js
@@ -0,0 +1,23 @@
+const { validationResult, matchedData } = require("express-validator");
+const { StudentModel } = require("../models/studentModel");
+
+async function getStudent(req, res, next) {
+  try {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result });
+    }
+
+    const santisedId = matchedData(req).id;
+
+    const student = await StudentModel.findOne({ where: { id: santisedId } });
+    if(!student){
+        return res.sendStatus(404)
+    }
+    return res.status(200).send(student);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+}
+
+module.exports = getStudent;
--- routes/student.js
+++ routes/student.js
@@ -5,14 +5,21 @@ const { checkSchema } = require("express-validator");
 const createStudentSchema = require("../validation/createStudentSchema");
 const loginStudentSchema = require("../validation/loginStudentSchema");
 const getAllStudents = require("../controllers/getAllStudentsCtrl");
-
+const getStudent = require("../controllers/getStudentCtrl");
+const { param } = require("express-validator");
 
 const router = express.Router();
 
-router.post("/signup",checkSchema(createStudentSchema), createStudent);
+router.post("/signup", checkSchema(createStudentSchema), createStudent);
 
-router.post("/login", checkSchema(loginStudentSchema),loginStudent);
+router.post("/login", checkSchema(loginStudentSchema), loginStudent);
 
-router.get("/students",getAllStudents)
+router.get("/students", getAllStudents);
+
+router.get(
+  "/students/:id",
+  param("id").isNumeric().withMessage("Id should be number"),
+  getStudent
+);
 
 module.exports = router;
